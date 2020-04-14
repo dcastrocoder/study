c3f25ba22ee7757990fafb89ebdb71a59b6e03c8
dcastrocoder <jonathandelcas@gmail.com>
2020-04-13 22:04:19 -0500

    expresiones con JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = <h1>Hola mundo</h1>;
+const Datos = {
+  Nombre: "Jhonatan ",
+  Apellido: "Delgado",
+  Edad: "33",
+};
+
+function getInfo(user) {
+  return `Esta es otra manera. ${user.Nombre} ${user.Apellido}`;
+}
+
+const App = (
+  <div>
+    <h1>
+      Hola mundo, mi nombre es {Datos.Nombre} {Datos.Apellido}
+    </h1>
+    <p>Mi edad multiplicada es {Datos.Edad * 2}</p>
+    <p>{getInfo(Datos)}</p>
+  </div>
+);
 
 const root = document.getElementById("root");
 
