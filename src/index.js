0ba44f641ae6473737d37ffb30bff79301d2612b
dcastrocoder <jonathandelcas@gmail.com>
2020-04-13 23:30:48 -0500

    Crear un componente con Estado usando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const FrutasFrescas = (props) => (
-  <div>
-    <h1>{props.name} </h1>
-    <h3>{props.descripcion} </h3>
-    <h3>${props.precio} </h3>
-  </div>
-);
+class FrutasFrescas extends React.Component {
+  // es lo primero que se ejecuta
+  // las propiedades dentro d una clase siempre se refieren a ellas como this
+  constructor() {
+    super();
+    this.state = {
+      cantidad: 10,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name} </h1>
+        <h3>{this.props.descripcion} </h3>
+        <h3>Cantidad: {this.state.cantidad}</h3>
+        <button
+          onClick={() => {
+            this.setState({ cantidad: this.state.cantidad + 1 });
+            // setState se usa para actualizar estado del componente
+          }}
+        >
+          Agregar
+        </button>
+        <h3>${this.props.precio} </h3>
+      </div>
+    );
+  }
+}
 
 const App = () => (
   <div>
