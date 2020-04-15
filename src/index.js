ba3e8dc6f3643a7ce5f04bbf6688e79ba7fc8497
dcastrocoder <jonathandelcas@gmail.com>
2020-04-14 20:24:16 -0500

    Usando metodos de clase para manejar estados

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class FrutasFrescas extends React.Component {
-  // es lo primero que se ejecuta
-  // las propiedades dentro d una clase siempre se refieren a ellas como this
+class TarjetaFruta extends React.Component {
   constructor() {
     super();
+
+    const METHODS = ["agregar", "quitar", "limpiar"];
+
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this);
+    });
+
     this.state = {
-      cantidad: 10,
+      cantidad: 0,
     };
+  }
+
+  agregar() {
+    this.setState({
+      cantidad: this.state.cantidad + 1,
+    });
+  }
+
+  quitar() {
+    this.setState({
+      cantidad: this.state.cantidad - 1,
+    });
+  }
+
+  limpiar() {
+    this.setState({
+      cantidad: 0,
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>{this.props.name} </h1>
-        <h3>{this.props.descripcion} </h3>
-        <h3>Cantidad: {this.state.cantidad}</h3>
-        <button
-          onClick={() => {
-            this.setState({ cantidad: this.state.cantidad + 1 });
-            // setState se usa para actualizar estado del componente.
-          }}
-        >
-          Agregar
-        </button>
-        <h3>${this.props.precio} </h3>
+        <h3>{this.props.name}</h3>
+        <div>Cantidad: {this.state.cantidad}</div>
+        <button onClick={this.agregar}> + </button>
+        <button onClick={this.quitar}> - </button>
+        <button onClick={this.limpiar}> Limpiar</button>
+        <hr />
+        <p>$ {this.props.price}</p>
       </div>
     );
   }
@@ -33,12 +51,10 @@ class FrutasFrescas extends React.Component {
 
 const App = () => (
   <div>
-    <FrutasFrescas name="Manzana" descripcion="dulce" precio={2.0} />
-    <FrutasFrescas name="Lulo" descripcion="ácido" precio={2.0} />
-    <FrutasFrescas name="Pera" descripcion="dulce" precio={2.0} />
+    <TarjetaFruta name={"Sandia"} price={5.0} />
+    <TarjetaFruta name={"Naranja"} price={1.5} />
+    <TarjetaFruta name="Kiwi" price={3.3} />
   </div>
 );
 
-const root = document.getElementById("root");
-
-ReactDOM.render(<App />, root);
+ReactDOM.render(<App />, document.getElementById("root"));
